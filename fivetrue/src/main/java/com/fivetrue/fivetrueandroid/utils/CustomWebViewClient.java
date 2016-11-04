36165ced2ba9a591c3d981bd360776de386a0654
fivetrue <fivetrue1101@gmail.com>
2016-11-04 15:45:11 +0900

    update Library

--- fivetrue/src/main/java/com/fivetrue/fivetrueandroid/utils/CustomWebViewClient.java
+++ fivetrue/src/main/java/com/fivetrue/fivetrueandroid/utils/CustomWebViewClient.java
@@ -6,16 +6,12 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
-import android.util.Log;
-import android.view.View;
-import android.webkit.JavascriptInterface;
 import android.webkit.PermissionRequest;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
 
 import com.fivetrue.fivetrueandroid.ui.diaglog.LoadingDialog;
 
